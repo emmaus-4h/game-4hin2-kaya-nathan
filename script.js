2ca6d19677129ea2ec74b93829e37fff9a43cb42
N3th7 <93260554+N3th7@users.noreply.github.com>
2022-05-12 14:01:07 +0200

    Nathan werkend

--- script.js
+++ script.js
@@ -17,6 +17,24 @@ var spelStatus = SPELEN;
 var spelerX = 600; // x-positie van speler
 var spelerY = 600; // y-positie van speler
 
+var kogelX = 600; // x-positie van kogel
+var kogelY = 600; // y-positie van kogel
+
+var vijandX = 1300; // x-positie van vijand
+var vijandY = 1300;// y-positie van vijand
+
+
+
+var arrow_up = 38;
+var arrow_down = 40;
+var arrow_left = 37;
+var arrow_right = 39;
+
+var speed_speler = 10;
+
+
+var shoot_ground = 69;
+var shoot_sky = 82;
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
 /* ********************************************* */
@@ -24,16 +42,56 @@ var spelerY = 600; // y-positie van speler
 /**
  * Updatet globale variabelen met posities van speler, vijanden en kogels
  */
-var beweegAlles = function () {
+var beweegAlles = function draw () {
   // speler
+ background('blue'); 
+  {
+    if (keyIsDown (arrow_left)) {
+    spelerX = spelerX - 10
+  }
+  if (keyIsDown(arrow_up)) {
+    spelerY = spelerY - 10
+  }
+    
+ /*valt naar beneden*/
+  else 
+  {spelerY = spelerY + 5 } 
+         
+  if (keyIsDown (arrow_right)) {
+    spelerX = spelerX + 10
+  }
+  if (keyIsDown (arrow_down)) {
+    spelerY = spelerY + 10
+  }
+  };
+
+/*grond*/
+if  (spelerY > 694) {
+  spelerY = 694
+};   
+
+if  (spelerY < 1) {
+spelStatus = GAMEOVER;
+};
 
 
+ // vijand
+  vijandX = vijandX  - 10
+
+  if (vijandX < 0){
+    vijandX = 1280
+  };
+   
 
 
-  
-  // vijand
 
   // kogel
+ 
+
+    
+
+  
+  
 };
 
 /**
@@ -43,7 +101,15 @@ var beweegAlles = function () {
  */
 var verwerkBotsing = function () {
   // botsing speler tegen vijand
-
+if (spelerX - vijandX < 50 &&
+   spelerX - vijandX >-50 &&
+   spelerY - vijandY < 50 &&
+   spelerY - vijandY >-50)
+ {
+   console.log("bots");
+   spelStatus = GAMEOVER;
+ 
+}
   // botsing kogel tegen vijand
 
   // update punten en health
@@ -55,18 +121,24 @@ var verwerkBotsing = function () {
  */
 var tekenAlles = function () {
   // achtergrond
-
+ background('blue');
+  
   // vijand
 
+  fill("black");
+  rect(vijandX - 25, vijandY - 25, 50, 50);
+
+  
+
+  
   // kogel
+ 
+
 
   // speler
   fill("white");
   rect(spelerX - 25, spelerY - 25, 50, 50);
-  fill("black");
-  ellipse(spelerX, spelerY, 10, 10);
-rect(0,690,1280,200);
-  rect(0,0,1280,30)
+
   // punten en health
 
 };
@@ -95,7 +167,7 @@ function setup() {
 
   // Kleur de achtergrond blauw, zodat je het kunt zien
   background('blue');
-}
+};
 
 /**
  * draw
@@ -112,6 +184,8 @@ function draw() {
     }
   }
   if (spelStatus === GAMEOVER) {
-    // teken game-over scher
+    // teken game-over scherm\
+    text("Reload voor nieuwe game", 100, 100)
+
   }
-}
+};
